15c585ae6e749766ddfada959c687488e597cc61
Carlos Melgares <melgarescarlos@gmail.com>
2022-09-19 19:04:31 +0200

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './assets/Spajzlogo22corto.png'
+import './App.css'
+import LanguageSelector from "./components/LanguageSelector"
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <img src={logo} className='logo'/>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Resources</a></li>
+        </ul>
+        <LanguageSelector />
+      </div>
+
+      <div className='content'>
+        <h1>Take the control of your Business</h1>
+        <p>Try the best order system of the market Y empieza a ahorrar un dinerillo</p>
+        <div>
+          <button type='button'><span></span>Restaurants</button>
+          <button type='button'><span></span>Suppliers</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/LanguageSelector.css";
+
+export default function LanguageSelector() {
+  return (
+    <select name="" id="">
+        <option value="en">ENGLISH</option>
+        <option value="hu">MAGYAR</option>
+        <option value="es">ESPANOL</option>
+    </select>
+  )
+}
